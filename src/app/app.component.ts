2888795e0b819b9ea2248da809070c7eb3ec415b
gizmo9411 <116691194+gizmo9411@users.noreply.github.com>
2024-05-31 21:10:42 -0400

    styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,39 +12,47 @@ import { CameraWheelComponent } from './components/camera-wheel/camera-wheel.com
   standalone: true,
   imports: [CommonModule, RouterOutlet, PhotoSelectionContainerComponent, CameraWheelComponent],
   template: `
+  <h1>The Curious Martian Rover</h1>
   <div id = "camera-wheel-wrapper">
-  <app-camera-wheel [cameraNames] = "cameraNameArray" [isAvailable] = "isAvailable()"/> 
+  <app-camera-wheel [cameraNames] = "cameraNameArray" [isAvailable] = "isAvailable()" [cameraSelectionState] = "cameraSelectionState" (cameraChangeEvent)="onCameraSelection($event)"/> 
   </div>
+
+
+
   @for(photo of roverPhotos(); track photo.id){  
     <p>{{photo.id}} </p>}
             <app-photo-selection-container [roverPhotos]="roverPhotos"></app-photo-selection-container>
             <p>{{displayedPhoto.id}}</p>
             @for(photo of availableCameras(); track photo){  
     <p>{{photo}} </p>}
+    <p>{{this.cameraSelectionState()}} </p>
             `,
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent implements OnInit, OnDestroy {
   title = 'curious-martian-rover';
   displayedPhoto: RoverPhoto = { id: -1, sol: -1, camera: "", imgSrc: "", earthDate: "", seen: false, initIndex: -1, };
 
   //Camera Signal from obseravable
   availableCameras = this.photoServ.availableCameras;
-
   //list of camera names
   cameraNameArray = ["ALL", "FHAZ", "RHAZ", "MAST", "CHEMCAM", 'MAHLI', "MARDI", "NAVCAM"];
-
   //signal Map for if that camer is available, computed from available cameras.
   isAvailable: Signal<Record<string, boolean>> = computed(() => {
     let cameraAvailabilityMap: Record<string, boolean> = { "ALL": false, "FHAZ": false, "RHAZ": false, "MAST": false, "CHEMCAM": false, 'MAHLI': false, "MARDI": false, "NAVCAM": false };
     this.availableCameras().forEach(element => cameraAvailabilityMap[element] = true)
     return cameraAvailabilityMap
   });
+  //camera Current State
+  cameraSelectionState = signal("ALL");
 
   roverPhotos = this.photoServ.roverPhotos;
 
-  constructor(private photoServ: RoverPhotoRetrievalService) {
 
+
+  constructor(private photoServ: RoverPhotoRetrievalService) {
   }
 
   ngOnInit(): void {
@@ -53,18 +61,10 @@ export class AppComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
   }
 
-  onCameraSelection(camera: string): void {
-    this.photoServ.cameraSelected(camera);
+  onCameraSelection($event : Event): void {
+    console.log($event)
+    //this.photoServ.cameraSelected($event);
   }
 }
-
-// ALL
-// FHAZ
-// RHAZ
-// MAST
-// CHEMCAM
-// MAHLI
-// MARDI
-// NAVCAM
 
 
--- src/app/components/camera-wheel/camera-wheel.component.ts
+++ src/app/components/camera-wheel/camera-wheel.component.ts
@@ -1,27 +1,36 @@
-import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, Signal, signal, computed, EventEmitter, Output } from '@angular/core';
 import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-camera-wheel',
   standalone: true,
   imports: [CommonModule],
-  template:`  <div class ="spinner-shader" id="spinner-light"></div>
-  <div class ="spinner-shader" id="spinner-shadow"></div>
-  <div #panelRef id="activity-wheel-wrapper">
-    @for(camera of cameraNames; track camera;){
-      <div class="camera-panel" id ="camera{{$index}}" [style]="rotatePanel($index)" [ngClass] = "panelClass(camera)"><span>{{camera}}</span></div>
-    }
-  </div>
-   <div id="spinner-center"></div>
-   <div id="spinner-shroud"></div>
-    <div id="spinner-needle"></div>`,
+  template:`
+    <div class ="spinner-shader" id="spinner-light"></div>
+    <div class ="spinner-shader" id="spinner-shadow"></div>
+    <div #panelRef id="activity-wheel-wrapper" [style]="cameraRotation()">
+      @for(camera of cameraNames; track camera;){
+        <div class="camera-panel" id ="camera{{$index}}" [style]="rotatePanel($index)" [ngClass] = "panelClass(camera)" (click)="cameraPanelClick(camera)"><span (click)="cameraPanelClick(camera)">{{camera}}</span></div>
+      }
+    </div>
+    <div id="spinner-center"></div>
+    <div id="spinner-shroud"></div>
+      <div id="spinner-needle"></div>`,
   styleUrl: './camera-wheel.component.css'
 })
 export class CameraWheelComponent implements OnInit, AfterViewInit {
     @Input() cameraNames: string[] = [];
-    @Input()  isAvailable : Record<string, boolean> = {
-    }
-    cameraNameArray : string[] = [];
+    @Input() isAvailable : Record<string, boolean> = {};
+    @Input() cameraSelectionState = signal("ALL" as string);
+    @Output() cameraPanelClickEvent = new EventEmitter<string>;
+
+    //computed rotation of the wrapper div in order to properly display current camera
+    cameraRotation = computed(()=>{
+      if(this.isAvailable[this.cameraSelectionState()]){
+      const x = this.cameraNames.indexOf(this.cameraSelectionState()) * (360/this.cameraNames.length);
+      return`rotate: ${x}deg;`} else {return ""};
+    });
+    
 
     panelClass(camera: string) {
       return {
@@ -29,22 +38,26 @@ export class CameraWheelComponent implements OnInit, AfterViewInit {
       }
     }
 
-
     rotatePanel(x : number): string {
       let degreesOfArc = (360/this.cameraNames.length) * x;
       let y = `rotate: -${degreesOfArc}deg; zIndex : ${x};`
       return y;
     }
 
+    cameraPanelClick(x : string){
+      this.cameraPanelClickEvent.emit(x);
+      console.log("heyy")
+    }
+
     
 
 
     ngOnInit(){
+      setTimeout(()=>this.cameraSelectionState.set("NAVCAM"),1000)
     }
     
     ngAfterViewInit(): void {
     }
-    
 }
 
 
